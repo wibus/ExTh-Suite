8c4cf8553e83166c2d7d38da31b86e4832258248
William Bussiere <william.bussiere@gmail.com>
2016-05-11 15:58:32 -0400

    Removing foggy ambient material

--- ExAnimator/Animators/TheFruitChoreographer.cpp
+++ ExAnimator/Animators/TheFruitChoreographer.cpp
@@ -63,7 +63,7 @@ void TheFruitChoreographer::setup(const std::shared_ptr<StageSet>& stageSet)
     // Environment
     _backdrop.reset( new ProceduralSun() );
     stageSet->setBackdrop(std::shared_ptr<Backdrop>(_backdrop));
-    //stageSet->setAmbientMaterial(material::createInsulator(glm::dvec3(0.98), material::AIR_REFRACTIVE_INDEX, 0.20, 1.0, 0.75));
+    //stageSet->setAmbientMaterial(material::createInsulator(glm::dvec3(0.90), material::AIR_REFRACTIVE_INDEX, 0.04, 1.0));
 
     glm::dvec3 focusPos = glm::dvec3(0, 0, 1.0);
     glm::dvec3 camPos = focusPos + glm::dvec3(8, -15, 5) * 1.3;
